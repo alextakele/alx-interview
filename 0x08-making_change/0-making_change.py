d8ea93b3cf277925e01b497213bd995c0ef3660e
alexa221 <alextakele16@gmail.com>
2023-06-15 23:52:15 -0700

    coins game

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,30 +1,24 @@
-#!/usr/bin/python3
-"""
-Module 0-making_change
-"""
-
+#!/usr/bin/env python3
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed
-    to meet a given amount total.
-
-    Dyanimc Programmming Bottom Up Solution
-    """
     if total <= 0:
         return 0
-    # initialize list with maximum amount(which cannot be reached)
-    # this helps to calculate a new minimum
-    # e.g if total = 2, list = [3,3,3]
-    dp = [total + 1] * (total + 1)
-    # set minimum number of coins for amount 0
-    dp[0] = 0
 
-    # start from 1 because we know dp[0]
-    for amount in range(1, total + 1):
-        # test for every coin
-        for coin in coins:
-            if amount - coin >= 0:
-                dp[amount] = min(dp[amount], 1 + dp[amount - coin])
-    return dp[total] if dp[total] != total + 1 else -1
+    coins.sort(reverse=True)
+    min_coins = float('inf')
+    stack = [(total, 0, 0)]
+
+    while stack:
+        remaining, start, count = stack.pop()
+
+        if remaining == 0:
+            min_coins = min(min_coins, count)
+
+        for i in range(start, len(coins)):
+            coin = coins[i]
+            new_remaining = remaining - coin
+
+            if new_remaining >= 0 and count + new_remaining // coin + 1 < min_coins:
+                stack.append((new_remaining, i, count + 1))
+
+    return min_coins if min_coins != float('inf') else -1
