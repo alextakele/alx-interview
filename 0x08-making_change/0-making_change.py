b03b935a435556d17c92e5fada57249a2d3df9bb
alexa221 <alextakele16@gmail.com>
2023-06-16 00:14:20 -0700

    coin game

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,24 +1,38 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Defines function that determines the fewest number of coins to make change
+"""
+
 
 def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed to meet a given total
+
+    parameters:
+        coins [list or positive ints]:
+            the values of the coins in your possession
+            you can assume you have an infinite number of coins of all values
+        total [int]:
+            total amount of change to make
+            if total is 0 or less, return 0
+
+    returns:
+        the fewest number of coins to make the change
+        or -1 if the total change cannot be made with the given coins
+    """
     if total <= 0:
         return 0
-
-    coins.sort(reverse=True)
-    min_coins = float('inf')
-    stack = [(total, 0, 0)]
-
-    while stack:
-        remaining, start, count = stack.pop()
-
-        if remaining == 0:
-            min_coins = min(min_coins, count)
-
-        for i in range(start, len(coins)):
-            coin = coins[i]
-            new_remaining = remaining - coin
-
-            if new_remaining >= 0 and count + new_remaining // coin + 1 < min_coins:
-                stack.append((new_remaining, i, count + 1))
-
-    return min_coins if min_coins != float('inf') else -1
+    if len(coins) is 0:
+        return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
